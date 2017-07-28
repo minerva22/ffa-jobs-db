8d95a1c1337a2a7bb7f25b76a37233a52b81f278
minerva22 <minerva.moawad@gmail.com>
2017-07-28 11:50:31 +0300

    create ffajobs db as django application and create Connection/ Location models

---
+++ jobsdb/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.location_name
+   
+class Connection(models.Model):
+    location = models.ForeignKey(Location)
+    ip = models.CharField(max_length=200)
+    port = models.IntegerField(default=0)
+    user= models.CharField(max_length=200)
+    password= models.CharField(max_length=200)
+    mfdbname= models.CharField(max_length=200)
+    bfdbname= models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.ip
